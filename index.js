b9c58b7c1a42fd80129a32ec096fd92db2e3cca0
rubelmollik86 <rubelmollik86@gmail.com>
2023-11-17 15:51:38 +0600

    Introduction to jQuery

---
+++ index.js
@@ -0,0 +1,5 @@
+// document.querySelector("h1").innerHTML = "This is hi... from query selector..!";
+
+// Now do same thing using jQuery
+
+$("h1").text("exploring start jQuery with Anisul islam sir.....")
